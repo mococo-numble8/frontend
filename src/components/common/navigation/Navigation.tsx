0521134c0bbe05f599394f8e1a05dc02f2e9372e
yoonvelop <yoonvelop@gmail.com>
2022-11-10 22:31:23 +0900

    #3b058c 공통 컴포넌트 - pretendard, framer-motion 설치, 페이지 레이아웃/네비게이션/온보딩 페이지 생성

---
+++ src/components/common/navigation/Navigation.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Navigation.module.scss';
+
+const Navigation = () => {
+  return <nav className={styles.nav}>Navigation</nav>;
+};
+
+export default Navigation;
