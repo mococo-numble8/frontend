15b3f9e28cf9045ddd5f5c5eba136b06f979a78b
yoonvelop <yoonvelop@gmail.com>
2022-11-13 14:03:24 +0900

    #86d743 react-query client, devTool 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from 'api/query/query-client';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import HomePage from './pages/HomePage';
 import ComponentTestPage from './components/common/test/ComponentTestPage';
 import TestContainer from './components/common/test/TestContainer';
@@ -12,19 +15,22 @@ import OnboardingPage from 'pages/OnboardingPage';
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route
-        path="/test"
-        element={<ComponentTestPage>{<TestContainer />}</ComponentTestPage>}
-      />
-      <Route path="/modal" element={<ModalSamplePage />} />
-      <Route path="/components" element={<ComponentSamplePage />} />
-      <Route path="/onboarding" element={<OnboardingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/join" element={<JoinPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/test"
+          element={<ComponentTestPage>{<TestContainer />}</ComponentTestPage>}
+        />
+        <Route path="/modal" element={<ModalSamplePage />} />
+        <Route path="/components" element={<ComponentSamplePage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/join" element={<JoinPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 };
 
