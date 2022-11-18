d58cbd16fd779089c7e051ded9936c0f946edeb1
Sungho Noh <nightwing@naver.com>
2022-11-18 16:56:03 +0900

    #3b058c - avartar 컴포넌트

--- src/pages/ModalSamplePage.tsx
+++ src/pages/ModalSamplePage.tsx
@@ -1,6 +1,7 @@
 import Icon from 'components/common/Icon/Icon';
 import Modal from 'components/common/Modal/Modal';
 import { useModal } from 'hooks/ui/useModal';
+import Avatar from 'components/common/avatar/avatar';
 
 const ModalSamplePage: React.FC = () => {
   const { openModal, closeModal } = useModal();
@@ -9,6 +10,7 @@ const ModalSamplePage: React.FC = () => {
     <>
       <h1>ModalSamplePage</h1>
       <button onClick={openModal}>open modal</button>
+      <Avatar />
       <div>
         <Modal
           icon={<Icon name="warning" alt="warning" className="large" />}
